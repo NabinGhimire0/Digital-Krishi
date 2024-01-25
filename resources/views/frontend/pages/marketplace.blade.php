3aa41d98e7c32136fa2706adb8942141c46ccc99
Reeyas13 <reeyaskarki@gmail.com>
2024-01-25 17:43:20 +0545

    feat - /market place

---
+++ resources/views/frontend/pages/marketplace.blade.php
@@ -0,0 +1,111 @@
+<x-guest-layout>
+    <div style="background: rgba(106,169,108);
+    box-shadow: 0 8px 32px 0 rgba( 31, 38, 135, 0.37 );
+    backdrop-filter: blur( 0px );
+    -webkit-backdrop-filter: blur( 0px );
+">
+        @include('frontend/layouts/inc/nav')
+
+    </div>
+    <div class="marktetplace wrapper">
+        <div class="search">
+            <center>
+
+                <form action="">
+                    <input type="text" name="" id="">
+                    <button>search</button>
+                </form>
+            </center>
+        </div>
+        <main>
+            <div class="marketplace__grid">
+                <div>
+
+                    <div>
+                        <a href="">
+                            <div>
+                                <img src="{{asset('assets/images/corn-seed.png')}}" alt="">
+                            </div>
+                        </a>
+                        <center>
+
+                            <p>Corn</p>
+                            <p>Price : <span>Rs. 100</span></p>
+                        </center>
+                    </div>
+
+                </div>
+                <div>
+
+                    <div>
+                        <a href="">
+                            <div>
+                                <img src="{{asset('assets/images/corn-seed.png')}}" alt="">
+                            </div>
+                        </a>
+                        <center>
+
+                            <p>Corn</p>
+                            <p>Price : <span>Rs. 100</span></p>
+                        </center>
+                    </div>
+
+                </div>
+                <div>
+
+                    <div>
+                        <a href="">
+                            <div>
+                                <img src="{{asset('assets/images/corn-seed.png')}}" alt="">
+                            </div>
+                        </a>
+                        <center>
+
+                            <p>Corn</p>
+                            <p>Price : <span>Rs. 100</span></p>
+                        </center>
+                    </div>
+
+                </div>
+                <div>
+
+                    <div>
+                        <a href="">
+                            <div>
+                                <img src="{{asset('assets/images/corn-seed.png')}}" alt="">
+                            </div>
+                        </a>
+                        <center>
+
+                            <p>Corn</p>
+                            <p>Price : <span>Rs. 100</span></p>
+                        </center>
+                    </div>
+
+                </div>
+                <div>
+
+                    <div>
+                        <a href="">
+                            <div>
+                                <img src="{{asset('assets/images/corn-seed.png')}}" alt="">
+                            </div>
+                        </a>
+                        <center>
+
+                            <p>Corn</p>
+                            <p>Price : <span>Rs. 100</span></p>
+                        </center>
+                    </div>
+
+                </div>
+
+
+            </div>
+
+        </main>
+
+    </div>
+
+
+</x-guest-layout>
--- routes/web.php
+++ routes/web.php
@@ -121,17 +121,19 @@ Route::get("/contact-us", function () {
     return view('frontend.pages.contactUs');
 });
 Route::resource('posts', CommunityPostController::class);
-<<<<<<< HEAD
 
-=======
 Route::get('/dashboard', function () {
     return view('frontend.pages.dashboard.dashboard');
 });
->>>>>>> e12759c2e5b3d5811ccfa5b07d4f8dca19fcd6aa
 Route::get("/community", function () {
     
     return view('frontend.pages.community');
 });
+Route::get("/marketplace", function () {
+
+    return view('frontend.pages.marketplace');
+});
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
